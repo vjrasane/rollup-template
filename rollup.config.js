813055c28c386a284d1c829bfbe5aa71698a6913
Ville Räsänen <ville.rasanen@nokia.com>
2018-09-19 16:19:55 +0300

    Updated rollup config and gitignore

--- rollup.config.js
+++ rollup.config.js
@@ -10,57 +10,56 @@ import fs from 'fs'
 
 const production = process.env.NODE_ENV === 'production'
 
-/*
-* Reads dependencies from package.json and sets them as
-* external dependencies so there's no need to hard code them here
-*/
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 const ext = Object.keys(pkg.dependencies || {})
 
-export default {
-  // tell rollup which libraries it can expect to be present externally
-  external: [...ext, 'path', 'fs', 'os', 'assert', 'glob'],
+// tell rollup which libraries it can expect to be present externally
+const external = [...ext, 'path', 'fs', 'os', 'assert', 'glob']
+const plugins = [
+  progress(),
+  /*
+  * Resolve dependencies to be bundled with your code. This should
+  * only be babel-runtime and everything else should be left as
+  * an external dependency.
+  */
+  json(), // transforms imported JSON files to JavaScript
+  builtins(),
+  commonjs({
+    include: 'node_modules/**',
+    exclude: ['src/**', 'test/**']
+  }),
+  resolve(),
+  babel({
+    /*
+    * Babel has to be configured separately for rollup, because we cannot use module transformers with it, which
+    * in turn are required by Jest, which will read its configuration from .babelrc
+    */
+    babelrc: false,
+    presets: [['env', { modules: false }], 'flow'],
+    exclude: ['node_modules/**', '*.json'], // only transpile our source code
+    runtimeHelpers: true, // otherwise transform-runtime throws an error
+    plugins: [
+      'external-helpers',
+      'transform-runtime', // async-await requires this
+      'transform-class-properties',
+      'transform-object-rest-spread'
+    ]
+  }),
+  production && uglify(), // uglify only in production
+  filesize()
+]
+
+export default [{
   // input file for building the bundle
   input: 'src/main.js',
   // output bundle file
   output: {
     file: 'dist/main.js',
-    format: 'cjs' // usually the best choice
-    // add banner for command line tool
-    /* banner: '#! /usr/bin/env node' */
+    format: 'cjs', // usually the best choice
+    // banner for command line tool
+    banner: '#! /usr/bin/env node'
   },
   context: null, // silence 'this is undefined' warning
-  plugins: [
-    progress(),
-    /*
-   * Resolve dependencies to be bundled with your code. This should
-   * only be babel-runtime and everything else should be left as
-   * an external dependency.
-   */
-    json(), // transforms imported JSON files to JavaScript
-    builtins(),
-    commonjs({
-      include: 'node_modules/**',
-      exclude: ['src/**', 'test/**']
-    }),
-    resolve(),
-    babel({
-      /*
-     * Babel has to be configured separately for rollup, because we cannot use module transformers with it, which
-     * in turn are required by Jest, which will read its configuration from .babelrc
-     */
-      babelrc: false,
-      presets: [['env', { modules: false }], 'flow'],
-      exclude: ['node_modules/**', '*.json'], // only transpile our source code
-      runtimeHelpers: true, // otherwise transform-runtime throws an error
-      plugins: [
-        'external-helpers',
-        'transform-runtime', // async-await requires this
-        'transform-class-properties',
-        'transform-object-rest-spread'
-      ]
-    }),
-    production && uglify(), // uglify only in production
-    filesize()
-  ]
-}
+  external,
+  plugins
+}]
